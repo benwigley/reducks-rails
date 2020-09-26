fe34bbacc7dd6b0f51f6c213a9c5b3fe6d74fd02
Ben Wigley <benwigley2@gmail.com>
2020-09-26 16:44:48 -0500

    Build for production

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+import normalize from './libs/normalize.ts'
+import normalizedMerge from './libs/normalizedMerge.ts'
+import actionTypeModifiers from './libs/actionTypeModifiers'
 import registerCollections from './reducks/registerCollections'
-import ReducksBaseCollection from './reducks/ReducksBaseCollection'
+import {
+  ReducksBaseCollection,
+  setEntities as setEntitiesAction,
+  setEntitiesFromState as setEntitiesFromStateAction
+} from './reducks/ReducksBaseCollection'
 import initReducksRailsMiddleware from './middleware/initReducksRailsMiddleware'
 
 export {
+  // Reducks stuff
+  normalize,
+  normalizedMerge,
+  actionTypeModifiers,
   registerCollections,
+
+  setEntitiesAction,
+  setEntitiesFromStateAction,
   ReducksBaseCollection,
+
+  // Middleware stuff
   initReducksRailsMiddleware,
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,36 @@ const path = require('path')
 
 console.log('Directory: ', __dirname)
 
-module.exports = {
-  devtool: false,
-  entry: path.join(__dirname, 'src', 'index.js'),
-  externals: {
-    redux: {
-      commonjs: 'redux',
-      commonjs2: 'redux',
-      amd: 'redux',
-      root: 'redux'
-    }
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts)$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/,
+module.exports = env => {
+  return {
+    devtool: false,
+    entry: path.join(__dirname, 'src', 'index.js'),
+    externals: {
+      redux: {
+        commonjs: 'redux',
+        commonjs2: 'redux',
+        amd: 'redux',
+        root: 'redux'
       }
-    ]
-  },
-  output: {
-    filename: 'reducks-rails.js',
-    library: 'reducks-rails',
-    libraryTarget: 'umd',
-    globalObject: 'typeof self !== \'undefined\' ? self : this'
-  },
-  plugins: [
-    // somePluginEnabled && new SomePlugin()
-  ].filter(plugin => !!plugin) // filter out false items
+    },
+    mode: env.production ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)$/,
+          loaders: ['babel-loader'],
+          exclude: /node_modules/,
+        }
+      ]
+    },
+    output: {
+      filename: 'reducks-rails.js',
+      library: 'reducks-rails',
+      libraryTarget: 'umd',
+      globalObject: 'typeof self !== \'undefined\' ? self : this'
+    },
+    plugins: [
+      // somePluginEnabled && new SomePlugin()
+    ].filter(plugin => !!plugin) // filter out false items
+  }
 }
